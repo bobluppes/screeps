1587df180c17b4dbe7078372b45d0b0c6a6f8303
Bob Luppes <bobluppes@live.nl>
2022-01-23 15:33:54 +0100

    Dynamically scale creep body parts with energy cap

--- src/SpawnManager.ts
+++ src/SpawnManager.ts
@@ -1,3 +1,15 @@
+import { floor } from "lodash";
+
+class ProposedBody {
+  parts: BodyPartConstant[];
+  cost: number;
+
+  constructor(parts: BodyPartConstant[], cost: number) {
+    this.parts = parts;
+    this.cost = cost;
+  }
+}
+
 export class SpawnManager {
 
   private static get_available_sources(spawn: StructureSpawn) : Source[] {
@@ -12,12 +24,31 @@ export class SpawnManager {
     })
   }
 
+  private static compute_max_body(capacity: number) : ProposedBody {
+    let body: BodyPartConstant[] = [];
+    let cost = 0;
+
+    let basic_cost = 200;
+
+    for (let i = 0; i < floor(capacity/basic_cost); i++) {
+      body.push(WORK);
+      body.push(MOVE);
+      body.push(CARRY);
+      cost += basic_cost;
+    }
+
+    body.sort();
+
+    return new ProposedBody(body, cost);
+  }
+
   public static spawn_creeps(spawn: StructureSpawn) {
     if (spawn.room.find(FIND_MY_CREEPS).length < 2) {
-      if (spawn.store.getUsedCapacity(RESOURCE_ENERGY) >= 200) {
+      let body = this.compute_max_body(spawn.room.energyCapacityAvailable);
+      if (spawn.room.energyAvailable >= body.cost) {
         let name = `creep_` + Game.time;
         let source = this.get_available_sources(spawn)[0];
-        spawn.spawnCreep([WORK, MOVE, CARRY], name, {memory: {working: false, source: source.id}});
+        spawn.spawnCreep(body.parts, name, {memory: {working: false, source: source.id}});
         console.log(`Spawning ${name}`);
       }
     }
